59cd38a2dbac44586941e522b89ecde75595a624
ISAAC JOSEPH <CODEDPRESET247@GMAIL.COM>
2026-01-14 17:56:06 +0100

    Correct syntax errors and enhance code readability
    Fixed syntax errors and added comments for clarity.

--- day 01-05-python/main.py
+++ day 01-05-python/main.py
@@ -1,1 +1,26 @@
+# Day 1-5 (Merged) - Python Foundations
 
+#Variables and Print
+name= "Isaac"
+age="17"
+print("Name:", name)
+print("Age:", age)
+
+#Math operations
+a=10
+b=3
+print("Addition:", a+b)
+print("Division:" a/b)
+print("Power:", a**b)
+
+# Lists and loops
+numbers = [1,2,3,4,5]
+for n in numbers:
+  print("Number:", n, "Square:", n**2)
+
+#conditionals
+X+7
+if x %2==0:
+  print("Even Number")
+else:
+  print("Odd Number")
